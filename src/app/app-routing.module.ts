373c7be3dc17c9b40857660233f610961ef789c9
username <maxime.grazzini@hotmail.com>
2022-12-15 08:20:21 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'matchanalysis', loadChildren: () => import('./matchanalysis/matchanalysis.module').then(m => m.MatchanalysisModule)
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
