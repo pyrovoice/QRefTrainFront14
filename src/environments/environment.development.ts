5a2d9dc25ac7ab44e9d9397827f23cc53d5ec5dd
username <maxime.grazzini@hotmail.com>
2023-03-16 18:30:10 +0100

    Quiz result improvements

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:45398/',
+    version: '1.0.0'
+  };
---
+++ src/environments/environment.production.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://qreftrain.azurewebsites.net/',
+    version: '1.0.0'
+  };
